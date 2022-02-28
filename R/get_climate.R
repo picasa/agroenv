0cd0aef2460aed3a4daaeb65e8f84379a07bba62
Pierre Casadebaig <pierre.casadebaig@inra.fr>
2022-02-28 11:48:59 +0100

    updated for nasapower API change

--- R/get_climate.R
+++ R/get_climate.R
@@ -32,29 +32,34 @@ get_climate_safran <- function(lat, lon, year, path) {
 #' @param year year(s) of the climate data to be retrieved
 #' @export
 
-get_climate_nasapower <- function(lat, lon, year) {
+get_climate_nasapower <- function(lat, lon, year)
+
+{
 
   data_tmp <- nasapower::get_power(
     community = "AG",
     lonlat = c(lon, lat),
-    pars = c("T2M", "T2M_MAX", "T2M_MIN", "ALLSKY_SFC_SW_DWN", "PRECTOT", "T2MDEW", "RH2M", "WS10M"),
-    dates = c(paste0(min(year),"-01-01"), paste0(max(year),"-12-31")),
-    temporal_average = "DAILY"
-  )
+    pars = c("T2M", "T2M_MAX", "T2M_MIN", "ALLSKY_SFC_SW_DWN", "PRECTOTCORR",
+             "T2MDEW", "RH2M", "WS10M"),
+    dates = c(paste0(min(year), "-01-01"), paste0(max(year), "-12-31")),
+    temporal_api= "DAILY")
 
-  # parse mean elevation of grid cell from object attribute
-  elev <- readr::parse_number(stringr::str_split(attributes(data_tmp)$POWER.Elevation, "=")[[1]][2])
+  elev <- readr::parse_number(
+    stringr::str_split(attributes(data_tmp)$POWER.Elevation, "=")[[1]][2])
 
-  # interpolate missing data and compute PET
   data_climate <- data_tmp %>%
     dplyr::mutate_at(dplyr::vars(T2M:WS10M), dplyr::na_if, -99) %>%
-    dplyr::mutate_at(dplyr::vars(T2M:WS10M), zoo::na.approx, maxgap=4) %>%
+    dplyr::mutate_at(dplyr::vars(T2M:WS10M), zoo::na.approx, maxgap = 4) %>%
     dplyr::mutate(
-      id=paste0(sprintf("%03d", round(abs(LAT))), sprintf("%03d", round(abs(LON)))),
-      YYYYMMDD=lubridate::as_date(YYYYMMDD),
-      PET=et_penman_monteith(lat=LAT, lon=LON, elevation = elev, day = DOY, tmin=T2M_MIN, tmax=T2M_MAX, tdew=T2MDEW, rad=ALLSKY_SFC_SW_DWN, wind=WS10M)
-    ) %>%
-    dplyr::select(id, date=YYYYMMDD, TN=T2M_MIN, TX=T2M_MAX, TM=T2M, RR=PRECTOT, GR=ALLSKY_SFC_SW_DWN, PET)
+      id = paste0(sprintf("%03d", round(abs(LAT))), sprintf("%03d", round(abs(LON)))),
+      YYYYMMDD = lubridate::as_date(YYYYMMDD),
+      PET = et_penman_monteith(
+        lat = LAT, lon = LON, elevation = elev, day = DOY,
+        tmin = T2M_MIN, tmax = T2M_MAX, tdew = T2MDEW,
+        rad = ALLSKY_SFC_SW_DWN, wind = WS10M)) %>%
+    dplyr::select(
+      id, date = YYYYMMDD, TN = T2M_MIN, TX = T2M_MAX, TM = T2M,
+      RR = PRECTOTCORR, GR = ALLSKY_SFC_SW_DWN, PET)
 
   return(data_climate)
 
--- R/utils_global.R
+++ R/utils_global.R
@@ -1,5 +1,5 @@
 utils::globalVariables(c(
-  "ALLSKY_SFC_SW_DWN", "DOY", "GR", "LAT", "LON", "PET", "PRECTOT", "RR", "RS",
+  "ALLSKY_SFC_SW_DWN", "DOY", "GR", "LAT", "LON", "PET", "PRECTOTCORR", "RR", "RS",
   "T2M", "T2MDEW", "T2M_MAX", "T2M_MIN", "TM", "TN", "TX", "WS10M", "YYYYMMDD",
   "distance", "id", "lat", "lon", "station_id", "x"
   ))
